f86f82e1a0203ca15d03ed62c1b75f883d7fe96e
emanu <emanuilabrahja@gmail.com>
2022-06-01 22:45:47 -0400

    2nd commit

--- src/javabb13/JBProvaGit.java
+++ src/javabb13/JBProvaGit.java
@@ -5,6 +5,8 @@ public class JBProvaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ili njishja");
+
+	System.out.println("ili rrapatushja");
 	}
 
 }
